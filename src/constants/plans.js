17d65d6b0175db82aa9ff4e31da114182d827558
VNWK <vcharrington3@gmail.com>
2021-02-06 15:52:22 -0500

    plans constant imatar API data

---
+++ src/constants/plans.js
@@ -0,0 +1,32 @@
+export default {
+  standard: {
+    name: "Plan Estandar",
+    price: 29,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    details: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "Duis aute irure dolor in reprehenderit in voluptate",
+      "velit esse cillum dolore eu fugiat nulla pariatur.",
+      "Excepteur sint occaecat cupidatat non proident",
+      "Ut enim ad minim veniam",
+    ]
+  },
+  premium: {
+    standard: {
+      name: "Plan Premium",
+      price: 59,
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      details: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Duis aute irure dolor in reprehenderit in voluptate",
+        "velit esse cillum dolore eu fugiat nulla pariatur.",
+        "Excepteur sint occaecat cupidatat non proident",
+        "Ut enim ad minim veniam",
+      ]
+    }
+  }
+}
