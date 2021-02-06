248d89ec7a2cac827bc606141e3c0a32b4ff9ade
VNWK <vcharrington3@gmail.com>
2021-02-06 15:12:23 -0500

    inicializado routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 import { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
 import styles from './App.module.css'
 
 const App = () => {
   return (
-    <Fragment>
-      <Header />
-      <article className={styles.content}>
-
-      </article>
-      <Footer />
-    </Fragment>);
+    <Router>
+      <Fragment>
+        <Header />
+        <article className={styles.content}>
+          <Switch>
+            <Route path="/suscripcion" component={() => <p>suscripcion</p>} />
+            <Route path="/datos" component={() => <p>datos</p>} />
+            <Route path="/confirmacion" component={() => <p>confirmacion</p>} />
+            <Redirect to="/suscripcion" />
+          </Switch>
+        </article>
+        <Footer />
+      </Fragment>
+    </Router>);
 }
 
 export default App;
