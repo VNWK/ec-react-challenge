cccce49cad1a3960aabe032e7d62354b7d2e3c14
VNWK <vcharrington3@gmail.com>
2021-02-07 17:21:54 -0500

    conectado confirmacion con context

--- src/components/Confirmacion.js
+++ src/components/Confirmacion.js
@@ -1,23 +1,23 @@
 import styles from './Confirmacion.module.css';
-import plans from '../constants/plans'
 
-const Confirmacion = () => {
-  return <div className={styles.root}>
-    <div className={styles.header}>
-      <p>Bienvenido NOMBRE</p>
-      <p>has adquirido el</p>
-      <h1>{plans.standard.name}</h1>
-      <h3>/S {plans.standard.price} al mes</h3>
-    </div>
+const Confirmacion = ({ form, planSelected, planList }) => {
+  return (Object.keys(planList).length > 0 &&
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <p>Bienvenido {form.name}</p>
+        <p>has adquirido el</p>
+        <h1>{planList[planSelected].name}</h1>
+        <h3>/S {planList[planSelected].price} al mes</h3>
+      </div>
 
-    <p className={styles.description}>
-      {plans.standard.description}
-    </p>
-    <div className={styles.buttons}>
-      <button className={styles.btnPlan}>Ir a ver mi plan</button>
-      <button>Ir a elcomercio.pe</button>
-    </div>
-  </div>
+      <p className={styles.description}>
+        {planList[planSelected].description}
+      </p>
+      <div className={styles.buttons}>
+        <button className={styles.btnPlan}>Ir a ver mi plan</button>
+        <button onClick={() => window.location = 'https://elcomercio.pe'}>Ir a elcomercio.pe</button>
+      </div>
+    </div>)
 }
 
 export default Confirmacion
--- src/screens/Confirmacion/Confirmacion.js
+++ src/screens/Confirmacion/Confirmacion.js
@@ -1,12 +1,20 @@
+import { useContext } from 'react'
+
 import Checkmark from '../../layout/Checkmark'
 import ConfirmacionComponent from '../../components/Confirmacion'
 import BaseLayout from '../../layout/BaseLayout'
 
+import { FormContext } from '../../contexts/form'
+import { AppContext } from '../../contexts/app'
+
 const Confirmacion = (props) => {
+  const { form } = useContext(FormContext)
+  const { planSelected, planList } = useContext(AppContext)
+
   return (
     <BaseLayout uri={props.location.pathname}>
       <Checkmark />
-      <ConfirmacionComponent />
+      <ConfirmacionComponent form={form} planSelected={planSelected} planList={planList} />
     </BaseLayout>
 
   )
